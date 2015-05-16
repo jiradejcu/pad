3ce2752714b2f0688a55e30b6c87c0f9d304c33e
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2015-05-16 09:55:59 +0700

    change pad record constraint : no unique day constraint

--- app/Http/Requests/CreatePadRequest.php
+++ app/Http/Requests/CreatePadRequest.php
@@ -22,7 +22,7 @@ class CreatePadRequest extends Request {
 	public function rules()
 	{
 		return [
-			'day' => 'required|numeric|unique_with:patient_pad_record,admission_id,day',
+			'day' => 'required|numeric',
 		];
 	}
 
--- database/migrations/2015_05_13_170553_create_pad_records_table.php
+++ database/migrations/2015_05_13_170553_create_pad_records_table.php
@@ -14,12 +14,12 @@ class CreatePadRecordsTable extends Migration {
 	{
 		Schema::create('patient_pad_record', function(Blueprint $table)
 		{
+			$table->increments('record_id');
 			$table->integer('admission_id')->unsigned();
 			$table->integer('day')->unsigned();
 			$table->string('data1');
 			$table->timestamps();
 			
-			$table->primary(['admission_id','day']);
 			$table->foreign('admission_id')
 			->references('admission_id')
 			->on('patient_admission')
