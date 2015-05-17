db9c1940a7f206f8ed5454694ec0931a71fbae9d
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2015-05-17 11:56:40 +0700

    update to tabular pad record display

--- app/PadRecord.php
+++ app/PadRecord.php
@@ -13,4 +13,23 @@ class PadRecord extends Model {
 	public function patientAdmission(){
 		return $this->belongsTo('App\PatientAdmission', 'admission_id');
 	}
+
+	public function getAnxietyAttribute($value){
+		return self::convertTriState($value);
+	}
+
+	public function getDeliriumAttribute($value){
+		return self::convertTriState($value);
+	}
+
+	public function getDrugInteractAttribute($value){
+		return self::convertTriState($value);
+	}
+	
+	private static function convertTriState($value){
+		if(!isset($value))
+			return 'N/A';
+		else
+			return !empty($value) ? 'yes' : 'no';
+	}
 }
--- resources/views/pad/index.blade.php
+++ resources/views/pad/index.blade.php
@@ -5,14 +5,62 @@
 	@forelse ($padRecordList as $admission_id => $padRecords)
 	<h2>HN : {{ $padRecords['admission']->patient->HN }}</h2>
 	
+	<table id="padTable">
+	  <tbody>
+	  <tr>
+	     <td width="200px">Day</td>
+	     <td>Numeric Rating</td>
+	     <td>Behavioral Pain Scale</td>
+	     <td>Sedation Assessment</td>
+	     <td>Anxiety</td>
+	     <td>Delirium Assessment</td>
+	     <td>Drug Interaction</td>
+	  </tr>	
 	@forelse ($padRecords['padRecord'] as $padRecord)
-	Day {{ $padRecord->day }} : Numeric Rating -> {{ $padRecord->nr }} <br>
+	  <tr>
+	     <td width="100px">{{ $padRecord->day }}</td>
+	     <td>{{ $padRecord->nr }}</td>
+	     <td>{{ $padRecord->bps }}</td>
+	     <td>{{ $padRecord->rass }}</td>
+	     <td>{{ $padRecord->anxiety }}</td>
+	     <td>{{ $padRecord->delirium }}</td>
+	     <td>{{ $padRecord->drug_interact }}</td>
+	  </tr>
 	@empty
     <p>No record</p>
 	@endforelse
+	  </tbody>
+	</table>
 	<hr>
 	<a href="{{ url('/pad/'.$admission_id.'/create') }}" tabindex="1">Add</a>
 	@empty
 	    <p>No record</p>
-	@endforelse	
+	@endforelse
+	
+	<script type="text/javascript">
+	<!--
+	$(function() {
+	    var t = $('#padTable tbody').eq(0);
+	    var r = t.find('tr');
+	    var cols= r.length;
+	    var rows= r.eq(0).find('td').length;
+	    var cell, next, tem, i = 0;
+	    var tb= $('<tbody></tbody>');
+	 
+	    while(i<rows){
+	        cell= 0;
+	        tem= $('<tr></tr>');
+	        while(cell<cols){
+	            next= r.eq(cell++).find('td').eq(0);
+	            tem.append(next);
+	        }
+	        tb.append(tem);
+	        ++i;
+	    }
+	    $('#padTable').append(tb);
+	    $('#padTable').show();
+	});
+	//-->
+	</script>
+	
 @stop
