6db29b4c4662da1f9a64c418518b3069ef5895c2
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2015-09-03 00:01:12 +0700

    pivot statistic table

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -20,9 +20,14 @@ class StatisticController extends Controller
     {
         $sql = "SELECT type, COUNT(HN) AS cnt, SUM(is_male)/COUNT(HN) AS percent_male, AVG(age) AS avg_age, AVG(apache_ii) AS avg_apache_ii";
         $sql .= ", SUM(septic_shock)/COUNT(HN) AS percent_septic_shock, SUM(cardiogenic_shock)/COUNT(HN) AS percent_cardiogenic_shock";
+        $sql .= ", SUM(adrenal_shock)/COUNT(HN) AS percent_adrenal_shock, SUM(hypovolemic_shock)/COUNT(HN) AS percent_hypovolemic_shock";
+        $sql .= ", SUM(asthma_exacerbation)/COUNT(HN) AS percent_asthma_exacerbation, SUM(copd_exacerbation)/COUNT(HN) AS percent_copd_exacerbation";
         $sql .= ", SUM(aki)/COUNT(HN) AS percent_aki, SUM(death)/COUNT(HN) AS percent_death, SUM(ards)/COUNT(HN) AS percent_ards";
         $sql .= ", AVG(icu_stay) AS avg_icu_stay, AVG(hospital_stay) AS avg_hospital_stay";
-        $sql .= " FROM (SELECT p.HN, IF(p.sex='m',1,0) AS is_male, p.apache_ii, pa.age, pa.type, pa.septic_shock, pa.cardiogenic_shock, pa.aki, pa.death, pa.reason LIKE '%ARDS%' AS ards";
+        $sql .= " FROM (SELECT p.HN, IF(p.sex='m',1,0) AS is_male, p.apache_ii, pa.age, pa.type";
+        $sql .= ", pa.septic_shock, pa.cardiogenic_shock, pa.adrenal_shock, pa.hypovolemic_shock";
+        $sql .= ", pa.asthma_exacerbation, pa.copd_exacerbation";
+        $sql .= ", pa.aki, pa.death, pa.reason LIKE '%ARDS%' AS ards";
         $sql .= ", DATEDIFF(pa.icu_admission_date_to, pa.icu_admission_date_from) AS icu_stay";
         $sql .= ", DATEDIFF(pa.hospital_admission_date_to, pa.hospital_admission_date_from) AS hospital_stay";
         $sql .= " FROM patient p JOIN patient_admission pa USING(HN) WHERE p.apache_ii IS NOT NULL) A";
@@ -32,18 +37,26 @@ class StatisticController extends Controller
 
     public function patientPadMedStatistic()
     {
-        $sql = "SELECT type, med_name, format(AVG(med_dose_day), 2) AS avg_med_dose_day FROM (";
+        $sql = "SELECT med_name, SUM(CASE type WHEN 'prospective' THEN avg_med_dose_day ELSE 0 END) AS prospective";
+        $sql .= ", SUM(CASE type WHEN 'retrospective' THEN avg_med_dose_day ELSE 0 END) AS retrospective FROM (";
+
+        $sql .= "SELECT type, med_name, format(AVG(med_dose_day), 2) AS avg_med_dose_day FROM (";
         $sql .= "SELECT HN, type, med_name, SUM(final_med_dose) AS sum_med_dose, icu_stay, SUM(final_med_dose)/icu_stay AS med_dose_day FROM (";
         $sql .= "SELECT *, COALESCE(med_dose_drip, med_dose) AS final_med_dose FROM (";
         $sql .= "SELECT *, med_duration * med_dose_hr AS med_dose_drip FROM (";
+
         $sql .= "SELECT p.HN, p.apache_ii, pa.type, pa.death, pa.reason LIKE '%ARDS%' AS ards";
         $sql .= ", ppmr.med_record_id, ppmr.med_name, ppmr.med_dose, ppmr.med_dose_hr";
         $sql .= ", TIME_TO_SEC(TIMEDIFF(ppmr.med_time_to, ppmr.med_time_from))/3600 AS med_duration";
         $sql .= ", DATEDIFF(pa.icu_admission_date_to, pa.icu_admission_date_from) AS icu_stay";
         $sql .= " FROM patient p JOIN patient_admission pa USING(HN) JOIN patient_pad_record ppr USING(admission_id)";
         $sql .= " JOIN patient_pad_med_records ppmr ON ppr.record_id = ppmr.pad_record_id WHERE p.apache_ii IS NOT NULL) A) B) C";
+
         $sql .= " WHERE icu_stay > 0 GROUP BY HN, med_name) D";
         $sql .= " GROUP BY type, med_name";
+
+        $sql .= ") E GROUP BY med_name";
+
         return DB::select($sql);
     }
 
--- resources/views/statistic/index.blade.php
+++ resources/views/statistic/index.blade.php
@@ -1,55 +1,101 @@
 @extends('app')
 
 @section('content')
-    <table width="100%">
-        <tr>
-            <td>type</td>
-            <td>count</td>
-            <td>%male</td>
-            <td>avg age</td>
-            <td>avg apache ii</td>
-            <td>%septic shock</td>
-            <td>%cardiogenic shock</td>
-            <td>%aki</td>
-            <td>%death</td>
-            <td>%ards</td>
-            <td>avg icu stay</td>
-            <td>avg hospital stay</td>
-        </tr>
-        @forelse ($patients as $patient)
+    <table id="summary_statistic"  width="100%" border="1px black">
+    	<tbody>
             <tr>
-                <td>{{ $patient->type }}</td>
-                <td>{{ $patient->cnt }}</td>
-                <td>{{ number_format($patient->percent_male, 2) }}</td>
-                <td>{{ number_format($patient->avg_age, 2) }}</td>
-                <td>{{ number_format($patient->avg_apache_ii, 2) }}</td>
-                <td>{{ number_format($patient->percent_septic_shock, 2) }}</td>
-                <td>{{ number_format($patient->percent_cardiogenic_shock, 2) }}</td>
-                <td>{{ number_format($patient->percent_aki, 2) }}</td>
-                <td>{{ number_format($patient->percent_death, 2) }}</td>
-                <td>{{ number_format($patient->percent_ards, 2) }}</td>
-                <td>{{ number_format($patient->avg_icu_stay, 2) }}</td>
-                <td>{{ number_format($patient->avg_hospital_stay, 2) }}</td>
+                <td>type</td>
+                <td>count</td>
+                <td>%male</td>
+                <td>avg age</td>
+                <td>avg apache ii</td>
+                <td>%septic shock</td>
+                <td>%adrenal shock</td>
+                <td>%hypovolemic shock</td>
+                <td>%cardiogenic shock</td>
+                <td>%asthma exacerbation</td>
+                <td>%copd exacerbation</td>
+                <td>%aki</td>
+                <td>%death</td>
+                <td>%ards</td>
+                <td>avg icu stay</td>
+                <td>avg hospital stay</td>
             </tr>
-        @empty
-            <tr><td>No patient</td></tr>
-        @endforelse
+            @forelse ($patients as $patient)
+                <tr>
+                    <td>{{ $patient->type }}</td>
+                    <td>{{ $patient->cnt }}</td>
+                    <td>{{ number_format($patient->percent_male, 2) }}</td>
+                    <td>{{ number_format($patient->avg_age, 2) }}</td>
+                    <td>{{ number_format($patient->avg_apache_ii, 2) }}</td>
+                    <td>{{ number_format($patient->percent_septic_shock, 2) }}</td>
+                    <td>{{ number_format($patient->percent_adrenal_shock, 2) }}</td>
+                    <td>{{ number_format($patient->percent_hypovolemic_shock, 2) }}</td>
+                    <td>{{ number_format($patient->percent_cardiogenic_shock, 2) }}</td>
+                    <td>{{ number_format($patient->percent_asthma_exacerbation, 2) }}</td>
+                    <td>{{ number_format($patient->percent_copd_exacerbation, 2) }}</td>
+                    <td>{{ number_format($patient->percent_aki, 2) }}</td>
+                    <td>{{ number_format($patient->percent_death, 2) }}</td>
+                    <td>{{ number_format($patient->percent_ards, 2) }}</td>
+                    <td>{{ number_format($patient->avg_icu_stay, 2) }}</td>
+                    <td>{{ number_format($patient->avg_hospital_stay, 2) }}</td>
+                </tr>
+            @empty
+                <tr><td>No patient</td></tr>
+            @endforelse
+        </tbody>
     </table>
     <br>
-    <table width="100%">
-        <tr>
-            <td>type</td>
-            <td>med name</td>
-            <td>avg med dose per day</td>
-        </tr>
-        @forelse ($padMedRecords as $padMedRecord)
+    <table width="100%" border="1px black">
+        <tbody>
             <tr>
-                <td>{{ $padMedRecord->type }}</td>
-                <td>{{ $padMedRecord->med_name }}</td>
-                <td>{{ number_format($padMedRecord->avg_med_dose_day, 2) }}</td>
+                <td>med name</td>
+                <td>prospective</td>
+                <td>retrospective</td>
             </tr>
-        @empty
-            <tr><td>No patient</td></tr>
-        @endforelse
+            @forelse ($padMedRecords as $padMedRecord)
+                <tr>
+                    <td>{{ $padMedRecord->med_name }}</td>
+                    <td>{{ $padMedRecord->prospective }}</td>
+                    <td>{{ $padMedRecord->retrospective }}</td>
+                </tr>
+            @empty
+                <tr><td>No patient</td></tr>
+            @endforelse
+        </tbody>
     </table>
+@stop
+
+@section('footer')
+
+<script type="text/javascript">
+<!--
+$(function() {
+	transposeTable();
+});
+
+function transposeTable(){
+    var t = $('#summary_statistic tbody').eq(0);
+    var r = t.children('tr');
+    var cols= r.length;
+    var rows= r.eq(0).children('td').length;
+    var cell, next, tem, i = 0;
+    var tb= $('<tbody></tbody>');
+
+    while(i<rows){
+        cell= 0;
+        tem= $('<tr></tr>');
+        while(cell<cols){
+            next= r.eq(cell++).children('td').eq(0);
+            tem.append(next);
+        }
+        tb.append(tem);
+        ++i;
+    }
+    $('#summary_statistic').append(tb);
+    $('#summary_statistic').show();
+}
+//-->
+</script>
+
 @stop
