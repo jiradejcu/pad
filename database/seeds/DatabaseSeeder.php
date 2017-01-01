21f2649ef8c8c74305826e3cf5f25eddfa361be7
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2017-01-01 23:00:51 +0700

    seed patient admission

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -27,12 +27,31 @@ class DatabaseSeeder extends Seeder {
 		$reports = Excel::load(public_path() . '/data/Test.xlsx')->get();
 
 		foreach ($reports as $row) {
+			
 			$patient_data = [
-				'HN' => $row['hn']
+				'HN'  => $row['hn'],
+				'sex' => $row['gender'] == 'ชาย' ? 'm' : 'f',
+			];
+
+			$patient_admission_data = [
+				'admission_id'                 => $row['an'],
+				'HN'                           => $row['hn'],
+				'age'                          => $row['age'],
+				'hospital_admission_date_from' => $row['admit_date'],
+				'hospital_admission_date_to'   => $row['discharge_date']
 			];
 
 			$patient = Patient::firstOrNew($patient_data);
 			$patient->save();
+
+			$patient_admission = PatientAdmission::firstOrNew($patient_admission_data);
+
+			if ($row['transfer_ward_date_icu_to_others_ward'] && $patient_admission->icu_admission_date_from) {
+				$patient_admission->icu_admission_date_to = $row['transfer_ward_date_icu_to_others_ward'];
+				$patient_admission->icu_admission_date_from = $row['transfer_ward_date_icu_to_others_ward']->subHours($row['icu_stay_hours']);
+			}
+
+			$patient_admission->save();
 		}
 	}
 
