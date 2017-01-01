cfb3e7f19aeab2d8f6b7f5b1d996b434b0bc9b9b
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2017-01-01 18:33:40 +0700

    test seed patient data to db

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Patient;
+use App\PatientAdmission;
+
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -10,11 +13,27 @@ class DatabaseSeeder extends Seeder {
 	 *
 	 * @return void
 	 */
-	public function run()
-	{
+	public function run() {
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+		DB::table('patient_pad_med_records')->truncate();
+		DB::table('patient_pad_record')->truncate();
+		DB::table('patient_admission')->truncate();
+		DB::table('patient')->truncate();
+		DB::table('medicines')->truncate();
+		DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+		$reports = Excel::load(public_path() . '/data/Test.xlsx')->get();
+
+		foreach ($reports as $row) {
+			$patient_data = [
+				'HN' => $row['hn']
+			];
+
+			$patient = Patient::firstOrNew($patient_data);
+			$patient->save();
+		}
 	}
 
 }
