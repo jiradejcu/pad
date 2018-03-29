f4daa243427fdfb78b3dbbe25285194939b5101e
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2018-03-29 22:49:29 +0700

    move add patient button to top

--- resources/views/patient/index.blade.php
+++ resources/views/patient/index.blade.php
@@ -1,7 +1,12 @@
 @extends('app')
 
 @section('content')
-	<h1>Patient List</h1>	
+    <h1>
+        <div class="pull-right">
+            <a class="btn btn-large btn-primary" href="{{ url('/patient/create') }}" tabindex="1">+</a>
+        </div>
+        Patient List
+    </h1>
 	@forelse ($patientList as $patient)
 		<h2><a class="btn btn-large btn-danger" data-toggle="confirmation" id="{{ $patient->HN }}">x</a>
 		HN : <a href="{{ url('/patient/'.$patient->HN.'/edit') }}">{{ $patient->HN }}</a> <a href="{{ url('/patient/'.$patient->HN) }}">PAD</a></h2>
@@ -9,8 +14,6 @@
 	@empty
 	    <p>No patient</p>
 	@endforelse
-	<hr>
-	<a href="{{ url('/patient/create') }}" tabindex="1">Add</a>
 @stop
 
 @section('footer')
