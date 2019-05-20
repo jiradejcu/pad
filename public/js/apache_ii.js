0c493586d7c2b0b5057179e2d7e7f7a34a3d3942
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2019-05-20 19:39:34 +0700

    update score calculation function

--- public/js/apache_ii.js
+++ public/js/apache_ii.js
@@ -3,31 +3,51 @@ const input_list = [
     name: 'temperature',
     range: {
       include_equal: false,
-      initial_value: 1,
-      map: {
-        1: 2,
-        2: 3,
-        3: 4,
-        4: 5,
-      }
+      initial_value: 4,
+      map: [
+        {key: 30, value: 4},
+        {key: 32, value: 3},
+        {key: 34, value: 2},
+        {key: 36, value: 1},
+        {key: 38.5, value: 0},
+        {key: 39, value: 1},
+        {key: 41, value: 3},
+      ]
+    }
+  },
+  {
+    name: 'mean_arterial_pressure',
+    range: {
+      include_equal: false,
+      initial_value: 4,
+      map: [
+        {key: 50, value: 4},
+        {key: 70, value: 2},
+        {key: 110, value: 0},
+        {key: 130, value: 2},
+        {key: 160, value: 3},
+      ]
     }
   }
 ]
 
 const getScoreFromRange = function(input, range) {
-  return Object.keys(range.map).reduce(function(total, key) {
-    if ((range.include_equal && input >= key) || (!range.include_equal && input > key)) {
-      return range.map[key]
+  input = Number(input)
+  var result = range.initial_value;
+  range.map.some(function(item) {
+    if ((range.include_equal && input <= item.key) || (!range.include_equal && input < item.key)) {
+      result = item.value
+      return true
     }
-    return total
-  }, range.initial_value)
+  })
+  return result
 }
 
 $(function() {
   input_list.forEach(function(item) {
     $("[name='" + item.name + "']").change(function() {
       const score = getScoreFromRange($(this).val(), item.range)
-      console.log(score)
+      console.log('score: ' + score)
     })
   })
 });
