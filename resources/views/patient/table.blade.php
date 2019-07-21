7943f5ebd726c7fe73a6134ba146c94174e613b4
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2019-07-21 14:48:20 +0700

    show only specific column in table view

--- resources/views/patient/table.blade.php
+++ resources/views/patient/table.blade.php
@@ -8,15 +8,27 @@
         Patient List
     </h1>
     <a href="{{ url('/') }}">List View</a>
+    <?
+        $patientDisplayColumn = ['HN', 'firstname', 'lastname', 'sex'];
+        $patientAdmissionDisplayColumn = ['age', 'temperature', 'mean_arterial_pressure', 'heart_rate', 'respiratory_rate'];
+		$patientAdmissionDisplayColumn = array_merge($patientAdmissionDisplayColumn, ['fio2', 'aapo2', 'pao2', 'ph_choice', 'ph', 'hco3', 'serum_na', 'serum_k']);
+		$patientAdmissionDisplayColumn = array_merge($patientAdmissionDisplayColumn, ['creatinine', 'hematocrit', 'wbc', 'glasgow_coma', 'chronic_health_problem']);
+		$patientAdmissionDisplayColumn = array_merge($patientAdmissionDisplayColumn, ['platelet', 'bilirubin', 'map_or_vaso', 'creatinine_or_urine']);
+		$patientAdmissionDisplayColumn = array_merge($patientAdmissionDisplayColumn, ['apache_ii_score', 'sofa_score']);
+	?>
     @if (!empty($patientList))
     <table id="patient_table" width="100%" border="1px black" style="margin-top: 10px">
     	<thead>
             <tr>
                 @foreach ($patientList[0]->toArray() as $key => $value)
+                    @if(in_array($key, $patientDisplayColumn))
                     <td>{{ $key }}</td>
+                    @endif
                 @endforeach
                 @foreach ($patientList[0]->admissions()->first()->toArray() as $key => $value)
+                    @if(in_array($key, $patientAdmissionDisplayColumn))
                     <td>{{ $key }}</td>
+                    @endif
                 @endforeach
             </tr>
         </thead>
@@ -24,10 +36,14 @@
             @foreach ($patientList as $patient)
             <tr>
                 @foreach ($patient->toArray() as $key => $value)
+                    @if(in_array($key, $patientDisplayColumn))
                     <td>{{ $value }}</td>
+                    @endif
                 @endforeach
                 @foreach ($patient->admissions()->first()->toArray() as $key => $value)
+                    @if(in_array($key, $patientAdmissionDisplayColumn))
                     <td>{{ $value }}</td>
+                    @endif
                 @endforeach
             </tr>
             @endforeach
