30dd9eaa786670d3c9733d52d3071751b2e73acc
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2019-11-03 12:34:19 +0700

    change tetra state form control default to "not eval" and change N/A to "unable to access"

--- resources/views/form_control/tetra_state.blade.php
+++ resources/views/form_control/tetra_state.blade.php
@@ -1,8 +1,8 @@
 <?php $form_class = !empty($form_class) ? ' ' . $form_class : ''; ?>
 <div class="form-group">
 	{!! Form::label($radio_name, $label_text) !!}<br>
-	<label class="radio-inline">{!! Form::radio($radio_name, '0', null, ['class' => $form_class]) !!}Not Eval</label>
-	<label class="radio-inline">{!! Form::radio($radio_name, '-', true, ['class' => $form_class]) !!}N/A</label>
+	<label class="radio-inline">{!! Form::radio($radio_name, '0', true, ['class' => $form_class]) !!}Not Eval</label>
+	<label class="radio-inline">{!! Form::radio($radio_name, '-', null, ['class' => $form_class]) !!}Unable to Access</label>
 	<label class="radio-inline">{!! Form::radio($radio_name, -1, null, ['class' => $form_class]) !!}Negative</label>
 	<label class="radio-inline">{!! Form::radio($radio_name, 1, null, ['class' => $form_class]) !!}Positive</label>
 	@if(!empty($detail_text))
