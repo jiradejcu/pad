0aaabe18d99485044605c84b1c79de510c294fd0
Jiradej Vatanutanon <jiradej.v@obodroid.com>
2020-07-18 11:23:39 +0700

    config allow override all in httpd service

--- docker/install.sh
+++ docker/install.sh
@@ -6,3 +6,6 @@ HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 composer
+
+sed -i '/<Directory \"\/var\/www\/html\">/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/httpd/conf/httpd.conf
+systemctl restart httpd.service
