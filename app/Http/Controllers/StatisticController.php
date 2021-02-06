2b7f4aacd1d871dfc83b64bc28c2a9481bff7f15
Jiradej Vatanutanon <jiradej.v@obodroid.com>
2021-02-06 12:42:48 +0700

    add delirium day column

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -311,8 +311,10 @@ class StatisticController extends Controller
     private function padSQL()
     {
         $sql = "SELECT p.HN, p.firstname, p.lastname, TIMESTAMPDIFF(HOUR, pa.icu_admission_date_from, pa.icu_admission_date_to) / 24 AS icu_stay";
-        $sql .= " FROM patient p JOIN patient_admission pa USING(HN) JOIN patient_pad_record ppr USING(admission_id) GROUP BY p.HN";
-
+        $sql .= ", (SELECT SUM(delirium) FROM (SELECT admission_id, date_assessed, IF(MAX(delirium) = 1, 1, 0) AS delirium FROM patient_pad_record GROUP BY admission_id , DATE(date_assessed)) A";
+        $sql .= " WHERE pa.admission_id = A.admission_id GROUP BY admission_id) AS delirium_day";
+        $sql .= " FROM patient p JOIN patient_admission pa USING(HN) JOIN patient_pad_record ppr USING(admission_id) GROUP BY pa.admission_id ORDER BY p.HN";
+        // dd($sql);
         return DB::select($sql);
     }
 
